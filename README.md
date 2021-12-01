01456469fbf382ef2d474df996d29ff999cd576c
Moody0101 <azmoudh@gmail.com>
2021-12-01 22:38:18 +0100

    Animations added

--- README.md
+++
@@ -1,2 +0,0 @@
-# Blog_
-my Blog source code..
--- js/script.js
+++ js/script.js
@@ -4,16 +4,16 @@ const Sections = [...document.getElementsByTagName("nav"),
 ]
 const loading = document.getElementById("loading");
 const  loadingDiv = document.getElementsByClassName("container")[0];
-gmailbutton.onclick = () => {
-	return
-}
+const temp = [
+	...document.getElementsByTagName("h2"),	...document.getElementsByTagName("p")
+]
+var delay = 500;
+var animate;
 
 
 for(let i=0; i < Sections.length; i++) {
     Sections[i].style.display = "none";
 }
-
-
 
 setTimeout(() => {
     loadingDiv.style.display = "none";
@@ -23,4 +23,33 @@ setTimeout(() => {
     Sections[i].style.transition = "1s all ease";
 }
 }, 4000);
+
+
+
+
+for(let i=0; i < temp.length, !(temp[i] == undefined);i++) {
+	
+	if (i % 2 == 0) {
+		Animate = "slide-left"
+	} else {
+		Animate = "slide-right"
+	}
+    temp[i].setAttribute('data-sal', Animate);
+    temp[i].setAttribute('data-sal-repeat', null);
+    temp[i].setAttribute('data-sal-delay', `${delay}`);
+    temp[i].setAttribute('data-sal-easing', "ease"); 
+   	delay += 50
+}
+
+
+
+sal({
+    once: false,
+    threshold: 0.6
+});
+
+// gmailbutton.onclick = () => {
+// 	return
+// }
+
 
